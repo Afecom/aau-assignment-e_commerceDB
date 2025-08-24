2c6de4905bbb00edbfc9429128a3060f30f76161
Nuru Kamil <knuru472@gmail.com>
2025-08-24 06:55:59 +0300

    updated db environment for deployment

--- db/sequelize.js
+++ db/sequelize.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config()
 import dbconfig from './config.cjs'
-const dbconf = dbconfig['development']
+const dbconf = dbconfig['production']
 
 const sequelize = new Sequelize(dbconf)
 
